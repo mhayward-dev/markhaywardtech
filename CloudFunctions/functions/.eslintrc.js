dd1834774685a44ccbbe44c54c03e380a963838f
Mark Hayward <mark@markhayward.tech>
2021-09-13 22:23:24 +0100

    Working getSpotifyToken request

--- CloudFunctions/functions/.eslintrc.js
+++ CloudFunctions/functions/.eslintrc.js
@@ -1,14 +1,19 @@
 module.exports = {
-  root: true,
-  env: {
-    es6: true,
-    node: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "google",
-  ],
-  rules: {
-    quotes: ["error", "double"],
-  },
+    root: true,
+    env: {
+        es6: true,
+        node: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'google',
+    ],
+    rules: {
+        quotes: ['error', 'single'],
+        indent: ['error', 4],
+    },
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+    },
 };
--- CloudFunctions/functions/index.js
+++ CloudFunctions/functions/index.js
@@ -1,15 +1,40 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
+const axios = require('axios');
+const qs = require('qs');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// firebase emulators:start
 
-exports.getConfig = functions.https.onRequest(async (req, res) => {
-    res.json({result: 'Testing 123'});
-});
+exports.getSpotifyToken = functions
+    .region('europe-west2')
+    .https.onRequest(async (req, res) => {
+    // Supply a refresh token from the Spotify token request API
+    // Also allow us to hide our secrets in Firebase environment variables
+    // https://developer.spotify.com/documentation/general/guides/authorization-guide/#authorization-flows
+
+        const secrets = functions.config().markhaywardtech;
+        const basicAuth = Buffer.from(`${secrets.spotify_client_id}` +
+    `:${secrets.spotify_client_secret}`).toString('base64');
+        const data = qs.stringify({
+            'refresh_token': secrets.spotify_refresh_token,
+            'grant_type': 'refresh_token',
+        });
+
+        axios({
+            method: 'post',
+            url: 'https://accounts.spotify.com/api/token',
+            headers: {
+                'Authorization': `Basic ${basicAuth}`,
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            data: data,
+        })
+            .then((r) => {
+                res.status(200).send(r.data);
+            })
+            .catch((e) => {
+                console.log(e);
+                res.sendStatus(500);
+            });
+    });
 
 
