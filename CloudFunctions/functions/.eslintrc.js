776b1bf91673c5445ccb55288979cc43abca197b
Mark Hayward <mark@markhayward.tech>
2021-09-14 10:13:28 +0100

    Updated eslint rules

--- CloudFunctions/functions/.eslintrc.js
+++ CloudFunctions/functions/.eslintrc.js
@@ -1,19 +1,17 @@
 module.exports = {
-    root: true,
-    env: {
-        es6: true,
-        node: true,
-    },
-    extends: [
-        'eslint:recommended',
-        'google',
-    ],
-    rules: {
-        quotes: ['error', 'single'],
-        indent: ['error', 4],
-    },
-    parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: 'module',
-    },
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'google',
+  ],
+  rules: {
+    quotes: ['error', 'single'],
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
 };
